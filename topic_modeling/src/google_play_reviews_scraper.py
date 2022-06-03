24f69c565bccc1cff15680de6c1bdb56299ca1ce
Jenny Raikakou <jennyraikakou@Jennys-MacBook-Pro.local>
2022-06-03 20:05:33 +0200

    Initialize the notebook

---
+++ topic_modeling/src/google_play_reviews_scraper.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from google_play_scraper import Sort, reviews_all
+
+if __name__ == '__main__':
+
+    mobile_apps_ids = ["com.ubs.swidKXJ.android", "com.csg.cs.dnmb"]
+    files_names = ["ubs-mobile-app-reviews.csv", "credit-suisse-mobile-app-reviews.csv"]
+    for index, mobile_app in enumerate(mobile_apps_ids):
+        us_reviews = reviews_all(
+            mobile_app,
+            sleep_milliseconds=0,  # defaults to 0
+            lang='en',  # defaults to 'en'
+            country='us',  # defaults to 'us'
+            sort=Sort.NEWEST,  # defaults to Sort.MOST_RELEVANT
+        )
+
+        df = pd.DataFrame(np.array(us_reviews), columns=["review"])
+        df = df.join(pd.DataFrame(df.pop("review").tolist()))
+        df.head()
+        df.to_csv(f"../../data/{files_names[index]}")
